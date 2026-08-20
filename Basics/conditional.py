b8e1202d6c2665a5d99bcd953adfc666f10dff66
Nicole Casey M. Basanes <nicolecasey.basanes@gmail.com>
2026-08-20 12:22:57 +0800

    modified stuffs

--- Basics/conditional.py
+++ Basics/conditional.py
@@ -1,21 +1,25 @@
-student_name = input("Student Name: ")
+student_name = input("Student Name: ") #user name input
 
 try:
-    student_score = int(input("Student Score: "))
+    student_score = int(input("Student Score: ")) #user input score
 
 
     if 90 <= student_score <= 100:
-            print("Grade: A")
+            print(student_name,"received Grade A") #prints if score is 90-100
     elif  80 <= student_score <=89:
-            print("Grade:B")
+            print(student_name,"received Grade B") #prints if score is 80-89
     elif 70 <= student_score <=79:
-            print("Grade:C")
+            print(student_name,"received Grade C") #prints if score is 70-79
     elif  61 <= student_score <=69:
-            print("Grade:D")
+            print(student_name,"received Grade D") #prints if score is 61-69
     elif student_score <=60:
-            print("Grade: F")
+            print(student_name,"received Grade F") #prints if score is below 60
     else:
-        print("Invalid Grade")
+        print("Invalid Grade") #prints if score input is not an integer
 
 except ValueError:
-    print("Invalid Grade")
+    print("Invalid Grade") #prints if score input is invalid
+
+
+
+
